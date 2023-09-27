e8352528be54a0ec7fe4b00ac66e9d3572943e47
phunnama <ma.phunnama@gmail.com>
2023-09-27 19:11:48 +0700

    Add BMI Description

--- BMICalculator/BMICalculator/Form1.cs
+++ BMICalculator/BMICalculator/Form1.cs
@@ -23,7 +23,35 @@ namespace BMICalculator
             double height = double.Parse(h_textBox.Text);
             double BMI = Math.Round(weight / Math.Pow((height/100.0),2),2);
 
+            string bmi_text = "";
+
+            if (BMI >= 30)
+            {
+                bmi_text = "Extreamely Obese";
+            }
+            else if (BMI >= 25 && BMI < 30)
+            {
+                bmi_text = "Obese";
+            }
+            else if (BMI >= 23 && BMI < 25)
+            {
+                bmi_text = "Overweight";
+            }
+            else if (BMI >= 18.5 && BMI < 23)
+            {
+                bmi_text = "Normal";
+            }
+            else if (BMI > 0 && BMI < 18.5)
+            {
+                bmi_text = "Underweight";
+            }
+            else
+            {
+                MessageBox.Show("Something wrong, Please try again!");
+            }
+
             BMI_num.Text = BMI.ToString();
+            BMI_text.Text = bmi_text;
         }
     }
 }
